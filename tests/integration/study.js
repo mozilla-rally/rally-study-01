f73c1060325fa2cfbf786970930602a3b581e962
Robert Helmer <rhelmer@rhelmer.org>
2021-04-07 23:56:52 -0700

    add super simple example of hosting wikipedia page and examining browser console output

--- tests/integration/study.js
+++ tests/integration/study.js
@@ -2,11 +2,14 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+
+const assert = require("assert");
 const utils = require("./utils.js");
 const { By, until } = require("selenium-webdriver");
 const firefox = require("selenium-webdriver/firefox");
-const lineReader = require('line-reader');
-
+const fs = require('fs');
+const readline = require('readline');
+const { exception } = require("console");
 
 // The number of milliseconds to wait for some
 // property to change in tests. This should be
@@ -29,10 +32,29 @@ async function findAndAct(driver, element, action) {
   await driver.findElement(element).then(e => action(e));
 }
 
+async function processLineByLine(pattern, expectedCount) {
+  const fileStream = fs.createReadStream("integration.log");
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  let count = 0;
+  for await (const line of rl) {
+    if (line.includes(pattern)) {
+      count++;
+    }
+  }
+
+  assert.ok(count == expectedCount, `Expected pattern ${pattern} to be present on ${expectedCount} line*s) of browser console output`);
+}
+
+
 describe("Study Template integration test example", function () {
   // eslint-disable-next-line mocha/no-hooks-for-single-case
   beforeEach(async function () {
-    this.driver = await utils.getFirefoxDriver(false);
+    this.driver = await utils.getFirefoxDriver(true);
     await this.driver.installAddon("web-ext-artifacts/study.xpi");
     await this.driver.setContext(firefox.Context.CONTENT);
   });
@@ -42,27 +64,16 @@ describe("Study Template integration test example", function () {
     await this.driver.quit();
   });
 
-  it("successfully opens the study template options page on installation", async function () {
-    await this.driver.get("https://wikipedia.org");
+  it("successfully runs the study against test sites", async function () {
+    await this.driver.get(`file:///${__dirname}/sites/wikipedia/index.html`);
 
-    // Let's wait until the page is fully loaded and the title matches.
+    // Let"s wait until the page is fully loaded and the title matches.
     await this.driver.wait(
       until.titleIs("Wikipedia"),
       WAIT_FOR_PROPERTY
     );
 
-    await this.driver.get("https://youtube.com");
-
-    // Let's wait until the page is fully loaded and the title matches.
-    await this.driver.wait(
-      until.titleIs("YouTube"),
-      WAIT_FOR_PROPERTY
-    );
-
-    lineReader.eachLine("integration.log", (line) => {
-      if (line.includes("RS01")) {
-        console.log("test123", line);
-      }
-    });
+    expectedCount = 1;
+    await processLineByLine("RS01", expectedCount);
   });
 });
