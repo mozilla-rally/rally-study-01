d5f564edeab2bc7e2ee916edbce53ce7a1b62a5e
hamilton <hamilton.ulmer@gmail.com>
2021-04-20 09:43:01 -0700

    updates measurement to measurements

--- src/study.js
+++ src/study.js
@@ -11,7 +11,7 @@ function collectEventDataAndSubmit(rally, devMode) {
     // we send the payload using one schema, "RS01.event".
     // Once https://github.com/mozilla-rally/web-science/issues/33 is resolved,
     // we will change the collection schema (but keep this pipeline schema the same).
-    rally.sendPing("measurement", data);
+    rally.sendPing("measurements", data);
   }, {
       matchPatterns: ["<all_urls>"],
       privateWindows: false
